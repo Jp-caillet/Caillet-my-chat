8f30f6fe75ed57d5d78952da53ff28c2d87731d9
caillet <jp78920@hotmail.com>
2018-02-01 14:22:48 +0100

    ajout d'un utilisateur

--- public/client.js
+++ public/client.js
@@ -1,9 +1,7 @@
 const socket = io();
 
 
-const chatadd = (target) =>{
- 	
-};
+//envoie d'un message au server
 const sendEl = document.querySelector('#send');
 
 sendEl.addEventListener('click', (e) => {
@@ -17,12 +15,30 @@ sendEl.addEventListener('click', (e) => {
  	if (message.text.trim().length !== 0) {
  		socket.emit('chat-message', message);
  	}
- 	socket.on('chat-message', function (message) {
+    document.querySelector('#m').focus();
+});
+
+//reception d'un message du server
+socket.on('chat-message', function (message) {
  		const nouveauMessage = document.createElement("li");
- 		const textnode = document.createTextNode(message.text);
+ 		const textnode = document.createTextNode(message.username+': '+message.text);
  		nouveauMessage.appendChild(textnode);
  		document.querySelector('#messages').appendChild(nouveauMessage);
  		console.log(textnode); 
     });
-    document.querySelector('#m').focus();
-})
+
+//connection utilisateur
+const sendUser = document.querySelector('#logger');
+
+sendUser.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+ 	const user ={
+ 		username : document.querySelector('#login input').value
+ 	};
+ 	if (user.username.length > 0) {
+ 		socket.emit('user-login', user);
+ 		document.querySelector('#m').focus();
+ 	}
+    
+});
--- server.js
+++ server.js
@@ -8,23 +8,43 @@ const io = require('socket.io')(http);
 // On gère les requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
 app.use('/', express.static(__dirname + '/public'));
 
-// On lance le serveur en écoutant les connexions arrivant sur le port 3000
-
-
 io.on('connection', function(socket){
+	let loggedUser;
+	/**
+ 	* Liste des utilisateurs connectés
+ 	*/
+ 	const users = [];
+
+	/**
+ 	* Historique des messages
+ 	*/
+	const messages = [];
+
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+
+    /**
+   	* Connexion d'un utilisateur via le formulaire
+   	*/
+  	socket.on('user-login', function (user) {
+    	loggedUser = user;
+    	//users.push(loggedUser);
+    	console.log('user logged in : ' + loggedUser.username);
+  	});
     /**
    * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
    */
-  socket.on('chat-message', function (message) {
-    console.log('message : ' + message.text);
+  	socket.on('chat-message', function (message) {
+  	 message.username = loggedUser.username;
+    console.log('message de : ' + message.username);
     io.emit('chat-message', message);
-  });
-  
+  	});
+  	  
 });
+
+// On lance le serveur en écoutant les connexions arrivant sur le port 3000
 http.listen(3000, function(){
     console.log('Server is listening on *:3000');
 });
