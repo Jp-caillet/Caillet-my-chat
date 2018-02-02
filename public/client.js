6b4bafd38605e16f3d9273e906259ced49a95d5f
caillet <jp78920@hotmail.com>
2018-02-02 12:20:05 +0100

    refractor .js on es6 (conform with eslint)

--- public/client.js
+++ public/client.js
@@ -1,44 +1,54 @@
-const socket = io();
-
-
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
 //envoie d'un message au server
 const sendEl = document.querySelector('#send');
 
 sendEl.addEventListener('click', (e) => {
-	e.preventDefault();
-	
- 	const message ={
- 		text : document.querySelector('#m').value
- 	};
- 	socket.emit('char-message',message);
- 	document.querySelector('#m').value="";
- 	if (message.text.trim().length !== 0) {
- 		socket.emit('chat-message', message);
- 	}
-    document.querySelector('#m').focus();
+  e.preventDefault();
+
+  const message = {
+    'text': document.querySelector('#m').value
+  };
+
+  socket.emit('char-message', message);
+  document.querySelector('#m').value = '';
+  if (message.text.trim().length !== 0) {
+    socket.emit('chat-message', message);
+  }
+  document.querySelector('#m').focus();
+});
+/**
+ * Réception d'un message de service
+ */
+socket.on('service-message', (message) => {
+  const messageService = document.createElement('li');
+  const textnode = document.createTextNode(message.text);
+
+  messageService.appendChild(textnode);
+  document.querySelector('#messages').appendChild(messageService);
 });
 
 //reception d'un message du server
-socket.on('chat-message', function (message) {
- 		const nouveauMessage = document.createElement("li");
- 		const textnode = document.createTextNode(message.username+': '+message.text);
- 		nouveauMessage.appendChild(textnode);
- 		document.querySelector('#messages').appendChild(nouveauMessage);
- 		console.log(textnode); 
-    });
+socket.on('chat-message', (message) => {
+  const nouveauMessage = document.createElement('li');
+  const textnode = document.createTextNode(message.username + ': ' + message.text);
+
+  nouveauMessage.appendChild(textnode);
+  document.querySelector('#messages').appendChild(nouveauMessage);
+});
 
 //connection utilisateur
 const sendUser = document.querySelector('#logger');
 
 sendUser.addEventListener('click', (e) => {
-	e.preventDefault();
-	
- 	const user ={
- 		username : document.querySelector('#login input').value
- 	};
- 	if (user.username.length > 0) {
- 		socket.emit('user-login', user);
- 		document.querySelector('#m').focus();
- 	}
-    
+  e.preventDefault();
+
+  const user = {
+    'username': document.querySelector('#login input').value
+  };
+
+  if (user.username.length > 0) {
+    socket.emit('user-login', user);
+    document.querySelector('#m').focus();
+  }
 });
--- server.js
+++ server.js
@@ -2,49 +2,58 @@
 // et la bibliothèque http integrée à node.
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-// On gère les requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
-app.use('/', express.static(__dirname + '/public'));
+// On gère les requêtes http des utilisateurs en leur renvoyant les fichiers du dossier 'public'
+app.use('/', express.static(`${__dirname}/public`));
 
-io.on('connection', function(socket){
-	let loggedUser;
-	/**
- 	* Liste des utilisateurs connectés
- 	*/
- 	const users = [];
+io.on('connection', (socket) => {
+  let loggedUser;
+  /**
+   * Déconnexion d'un utilisateur : broadcast d'un 'service-message'
+   */
 
-	/**
- 	* Historique des messages
- 	*/
-	const messages = [];
+  socket.on('disconnect', () => {
+    if (loggedUser) {
+      console.log('user disconnected : ' + loggedUser.username);
+      var serviceMessage = {
+        'text': 'User "' + loggedUser.username + '" disconnected',
+        'type': 'logout'
+      };
 
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+      socket.broadcast.emit('service-message', serviceMessage);
+    }
+  });
 
-    /**
-   	* Connexion d'un utilisateur via le formulaire
+  /**
+   	* connexion d'un utilisateur via le formulaire
    	*/
-  	socket.on('user-login', function (user) {
-    	loggedUser = user;
-    	//users.push(loggedUser);
-    	console.log('user logged in : ' + loggedUser.username);
-  	});
-    /**
+  socket.on('user-login', (user) => {
+    loggedUser = user;
+    if (loggedUser) {
+      var serviceMessage = {
+        'text': 'User "' + loggedUser.username + '" logged in',
+        'type': 'login'
+      };
+
+      console.log(serviceMessage.text);
+      socket.broadcast.emit('service-message', serviceMessage);
+    }
+  });
+
+  /**
    * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
    */
-  	socket.on('chat-message', function (message) {
-  	 message.username = loggedUser.username;
+  socket.on('chat-message', (message) => {
+    message.username = loggedUser.username;
     console.log('message de : ' + message.username);
     io.emit('chat-message', message);
-  	});
-  	  
+  });
 });
 
 // On lance le serveur en écoutant les connexions arrivant sur le port 3000
-http.listen(3000, function(){
-    console.log('Server is listening on *:3000');
+server.listen(3000, () => {
+  console.log('Server is listening on *:3000');
 });
